d58a2d3e2aed925684d7e33fb17cd3d8d7349867
Dany <danyshaanan@gmail.com>
2014-07-26 19:51:01 +0300

    removed semicolons

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,8 @@ module.exports = function(grunt) {
           quotmark: 'single',
           undef: true,
           trailing: true,
-          lastsemic: true
+          lastsemic: true,
+          asi: true
         }
       }
     }
